ebd997a024ac940a841c717128ad143bfcca1c61
Jack Donaghy <187638989+JackWilliamDonaghy-Dev@users.noreply.github.com>
2026-01-07 11:08:14 +0000

    Main late commit, Q1(c-i)

--- OOP_WinterExam20252026_KMcManus/Q1/MainWindow.xaml.cs
+++ OOP_WinterExam20252026_KMcManus/Q1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,7 +20,35 @@ namespace Q1
         public MainWindow()
         {
             InitializeComponent();
+            startData();
         }
+
+        ObservableCollection<Robot> robots = new ObservableCollection<Robot>();
+
+        public void startData()
+        {
+            tbckRobotInformation.Text = "";
+            lbxRobots.ItemsSource = robots; // change to filter using radio buttons
+
+            HouseholdRobot rb1 = new HouseholdRobot("HouseBot");
+            HouseholdRobot rb2 = new HouseholdRobot("GardenBot");
+            HouseholdRobot rb3 = new HouseholdRobot("Housemate 3000");
+            DeliveryRobot rb4 = new DeliveryRobot("DeliverBot");
+            DeliveryRobot rb5 = new DeliveryRobot("FlyBot");
+            DeliveryRobot rb6 = new DeliveryRobot("Driver");
+
+            rb2.DownloadSkill(HouseholdSkill.Gardening);
+            rb3.DownloadSkill(HouseholdSkill.Cooking);
+            rb3.DownloadSkill(HouseholdSkill.Laundry);
+
+            robots.Add(rb1);
+            robots.Add(rb2);
+            robots.Add(rb3);
+            robots.Add(rb4);
+            robots.Add(rb5);
+            robots.Add(rb6);
+        }
+
 
         private void HandleCheck(object sender, RoutedEventArgs e)
         {
@@ -29,7 +58,29 @@ namespace Q1
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (lbxRobots.SelectedItem is Robot selectedRobot)
+            {
+                tbckRobotInformation.Text = selectedRobot.RobotInfo();
+            }
+        }
 
+        private void btnCharge_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxRobots.SelectedItem is Robot selectedRobot)
+            {
+                if (selectedRobot.CurrentPowerKWH >= selectedRobot.PowerCapacityKWH)
+                {
+                    MessageBox.Show("Robot is already fully charged.");
+                    return;
+                }
+                else
+                {
+                    selectedRobot.CurrentPowerKWH = selectedRobot.PowerCapacityKWH;
+                    MessageBox.Show("Robot fully charged. Time taken: 3 hours and 20 minutes.");
+                    return;
+                }
+            }
+            
         }
     }
 }
---
+++ OOP_WinterExam20252026_KMcManus/Q1/Robot.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+
+    public enum HouseholdSkill
+    {
+        Cooking,
+        Cleaning,
+        Laundry,
+        Gardening,
+        ChildCare
+    }
+
+    public enum DeliveryMode
+    {
+        Walking,
+        Driving,
+        Flying
+    }
+
+
+    abstract class Robot
+    {
+        public string RobotName { get; set; }
+        public double PowerCapacityKWH { get; set; }
+        public double CurrentPowerKWH { get; set; }
+
+        public double GetBatteryPercentage()
+        {
+            if (PowerCapacityKWH == 0) return 0;
+            return (CurrentPowerKWH / PowerCapacityKWH) * 100;
+        }
+
+        public string DisplayBatteryInformation()
+        {
+            return $"Battery Information\nCapacity: {PowerCapacityKWH}kWh\nCurrent Power:{CurrentPowerKWH}kWh\nBattery Level: {GetBatteryPercentage():F2}%";
+        }
+
+        public abstract string DescribeRobot();
+
+        public string RobotInfo()
+        {
+            return $"{DescribeRobot()}";
+        }
+
+
+        public Robot(string robotName, double powerCapacityKWH, double currentPowerKWH)
+        {
+            RobotName = robotName;
+            PowerCapacityKWH = powerCapacityKWH;
+            CurrentPowerKWH = currentPowerKWH;
+        }
+
+        public Robot(string robotName)
+        {
+            RobotName = robotName;
+            PowerCapacityKWH = 100.0;
+        }
+
+        public Robot()
+        {
+            RobotName = "unknown";
+        }
+    }
+
+    class HouseholdRobot : Robot
+    {
+        private List<HouseholdSkill> Skills { get; set; } = new List<HouseholdSkill>();
+        public override string DescribeRobot()
+        {
+            string skillsDescription = Skills.Count > 0 ? string.Join(", ", Skills) : "No skills";
+            return $"I am a household robot.\nI can help with chores around the house.\n\nHousehold Robot Skills:\n{skillsDescription}\n\n{DisplayBatteryInformation()}";
+        }
+
+        public void DownloadSkill(HouseholdSkill skill)
+        {
+            if (!Skills.Contains(skill))
+            {
+                Skills.Add(skill);
+            }
+        }
+        public override string ToString()
+        {
+            return $"{RobotName} - [HouseholdRobot]";
+        }
+
+        public HouseholdRobot(string robotName) : base(robotName)
+        {
+            RobotName = robotName;
+            DownloadSkill(HouseholdSkill.Cleaning);
+        }
+
+    }
+
+    class DeliveryRobot : Robot
+    {
+        public DeliveryMode ModeOfDelivery { get; set; }
+        public double MaxLoadKG { get; set; }
+
+        public override string DescribeRobot()
+        {
+            return $"I am a delivery robot.\nI specialise in delivery by {ModeOfDelivery}\n\nThe maximum load I can carry is {MaxLoadKG:f2} kg\n\n{DisplayBatteryInformation()}";
+        }
+
+        public override string ToString()
+        {
+            return $"{RobotName} - [HouseholdRobot]";
+        }
+
+        public DeliveryRobot(string robotName, DeliveryMode delivery, double maxLoad) : base(robotName)
+        {
+            RobotName = robotName;
+            ModeOfDelivery = delivery;
+            MaxLoadKG = maxLoad;
+        }
+
+        public DeliveryRobot(string robotName) : base(robotName)
+        {
+            RobotName = robotName;
+            ModeOfDelivery = DeliveryMode.Walking;
+            MaxLoadKG = 5.0;
+        }
+
+    }
+}
